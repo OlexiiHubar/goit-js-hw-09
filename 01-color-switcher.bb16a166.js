21c9c9f33fba757a597af12c9a8106c5b857af4c
OlexiiHubar <115034934+OlexiiHubar@users.noreply.github.com>
2023-04-27 14:32:51 +0000

    Deploying to gh-pages from @ OlexiiHubar/goit-js-hw-09@60f307671b7d0481cda527b173a0d537f9893138 🚀

--- 01-color-switcher.bb16a166.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};function e(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,intervalId=setInterval(e,1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(intervalId)})),t.stopBtn.disabled=!0;
-//# sourceMappingURL=01-color-switcher.bb16a166.js.map
